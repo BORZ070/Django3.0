aa68947e7c6edfe9922ac28e2568bdcf82b579cf
BORZ070 <shdgit07@gmail.com>
2023-07-19 11:45:36 +0300

    Update urls.py

--- Django3_project/urls.py
+++ Django3_project/urls.py
@@ -4,10 +4,12 @@ from mypage.views import start_views
 from mypage_2.views import info_views
 from mypage_year.views import year_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mypage.urls')),
     path('info', include('mypage_2.urls')),
     path('year', include('mypage_year.urls'))
+
 
 ]
