862087c20f91e21264cb236323c5eb870567c6e4
BORZ070 <shdgit07@gmail.com>
2023-07-24 16:27:38 +0300

    miniup

--- Django3_project/urls.py
+++ Django3_project/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
     
